86f8c309e9e103bf157d1212ba84a01ef1e5310e
bruce <k-h.t@hotmail.com>
2020-05-24 20:06:27 +0200

    Datenbankverbindung

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app     = express();
+var path    = require('path');
+var mysql   = require('mysql');
+
+
+
+app.use(express.static(path.join(__dirname, '/dist/CoLA')));
+
+
+
+app.get('/', function(req,res)
+{
+    res.sendFile('index.html', { root: __dirname+'/dist/CoLA'});
+});
+
+app.get('/test', function(req,res)
+{
+    var con = mysql.createConnection({
+        database: "20_Gruppe1_DB",
+        port: "20133",
+        host: "195.37.176.178",
+        user: "Gruppe1New",
+        password: "Z$RrjuBp3Q'a;A;2fwZW4:A+Cxxo9gLd"
+        });
+
+    con.connect(function(err)
+    {
+        if (err) throw err;
+        console.log("connected");
+
+        con.query("SELECT * FROM User",function(err,result)
+        {
+            if (err) throw err;
+
+            res.send(result);
+        });
+
+        con.end();
+
+    });
+});
+
+
+app.listen(8080,function(){
+    console.log('erfolg!');
+});
