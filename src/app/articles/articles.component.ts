8f0bff3186377b3232a84ade46a7b0d3235c3941
bruce <k-h.t@hotmail.com>
2020-06-22 10:48:32 +0200

    user fixed

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -21,33 +21,35 @@ export class ArticlesComponent implements OnChanges {
 
   inventories: any[];
 
-  user: {Benutzername: "Tobias"}
+  user: any= {Benutzername: "Tobias"};
   
 
+
   constructor(private articleService: ArticleService, private commentService: commentService, private inventoryService: InventoryService) {
-   this.commentService.getComments().subscribe(data => this.comments = data); 
+    this.commentService.getComments().subscribe(data => this.comments = data);
     this.articleService.getArticles().subscribe(data => this.allArticles = data);
     this.inventoryService.getInventory().subscribe(data => this.inventories = data);
   }
   ngOnChanges() {
-   this.articleService.getArticles().subscribe(data => this.allArticles = data);
-   this.inventoryService.getInventory().subscribe(data => this.inventories = data);
+    this.articleService.getArticles().subscribe(data => this.allArticles = data);
+    this.inventoryService.getInventory().subscribe(data => this.inventories = data);
   }
 
-  onClick(){
+  onClick() {
     console.log(this.user);
+    var othis = this;
     this.inventories.forEach(function (value) {
-      if (value.User_Benutzername = this.user.Benutzername) {
-       if (value.Menge > this.allArticles.find(x => x.Produktname === value.Artikel_Produktname).MaximaleAnzahl) {
-         console.log(value);
-       }
+      if (value.User_Benutzername = othis.user.Benutzername) {
+        if (value.Menge > othis.allArticles.find(x => x.Produktname === value.Artikel_Produktname).MaximaleAnzahl) {
+          console.log(value);
+        }
       }
-    }); 
+    });
     document.getElementById('modal').style.display = "flex";
 
   };
 
-  onClose(){
+  onClose() {
     document.getElementById('modal').style.display = "none";
   };
 
