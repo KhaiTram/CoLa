d171fae943372e7bf91a832f9cfdb28ef49369c8
CaptainPancake <tobiasronge92@gmail.com>
2020-06-27 12:02:24 +0200

    -new header -cookies added -authservice expanded

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,18 +8,25 @@ export class AuthService {
 
   public users = [];
   public legitUser;
+  public loginStatus = false;
   constructor(private UserService: UserService) { }
+
+  subcribeToData()
+  {
+    this.UserService.getUsers().subscribe(data => this.users = data);
+  }
 
   authenticateLogin(name,password)
   {
-    this.UserService.getUsers().subscribe(data => this.users = data);  
+     
     for(let user of this.users)
     {
       if(name === user.Benutzername)
       {
         if(password === user.Passwort)
         { 
-          this.legitUser = user;         
+          this.legitUser = user;  
+          this.loginStatus = true;       
           return true;
 
         }
@@ -30,6 +37,31 @@ export class AuthService {
   getCurrentUser()
   {
     return this.legitUser;
+  }
+
+  getLoginStatus()
+  {
+    return this.loginStatus;
+  }
+
+  nameLookUp(name)
+  {
+    for(let user of this.users)
+    {
+      if(name === user.Benutzername)
+      {
+        this.legitUser = user;     
+        this.loginStatus = true;    
+        return true;    
+      }
+    }
+    return false;
+  }
+
+  logout()
+  {
+    this.legitUser  = undefined;
+    this.loginStatus = false;
   }
 }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent implements OnInit {
 
-  constructor() { } 
+export class HeaderComponent implements OnInit
+ {
+  text = "Login";
   
-  ngOnInit() { }
+  constructor( private AuthService: AuthService) { }
+
+  ngOnInit(): void 
+  {
+    this.AuthService.subcribeToData();
+    setTimeout(() => {this.cookieLogin() }, 1000);
+  }
+
+  public getCurrentUser()
+  {
+    console.log(this.AuthService.getCurrentUser());
+  }
+
+  public login()
+  {      
+    const loginElement = <HTMLInputElement> document.getElementById("loginNameField");
+    const passwordElement = <HTMLInputElement> document.getElementById("pswField");
+    var username = loginElement.value;
+    var password = passwordElement.value;
+    var auth = false;
+    console.log(username);
+    console.log(password);
+
+    auth = this.AuthService.authenticateLogin(username, password);
+
+    if(auth)
+    {
+      alert("Erfolgreicher Login")
+      document.cookie = "username="+username; 
+      this.text = "Logout";
+    }
+    else
+    {
+      alert("Username oder Passwort falsch")
+    }  
+  }
+
+  public cookieLogin()
+  {
+    var coockieName = document.cookie;
+    var realName = coockieName.split('=')[1];
+    console.log(coockieName);
+    console.log(realName);
+    if(this.AuthService.nameLookUp(realName))
+    {
+      console.log("User logged in");
+      this.text = "Logout";
+    }
+    else
+    {
+      console.log("No User logged in")
+    }
+  }
+
+  public logout()
+  {
+    this.AuthService.logout();
+    this.text = "Login";
+    var currentCoockie = document.cookie;
+    document.cookie = currentCoockie + "; expires=Thu, 01 Jan 1970 00:00:00 UTC" 
+    location.reload();
+  }
+
+  public logButton()
+  {
+    if(this.AuthService.getLoginStatus())
+    {
+      this.logout();
+    }
+  }
+
 }
